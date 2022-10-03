73c3f2f58643ccbf02393a2582097f4f963ed7e4
LikoDay <114933956+LikoDay@users.noreply.github.com>
2022-10-03 22:30:02 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,77 @@
+basic.forever(function () {
+    for (let index = 0; index < 1; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+    }
+    for (let index = 0; index < 1; index++) {
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . . # #
+            . . . . #
+            . . . . .
+            `)
+    }
+    for (let index = 0; index < 1; index++) {
+        basic.showLeds(`
+            . . . # .
+            . . # # #
+            . . # # #
+            . . . # #
+            . . . . #
+            `)
+    }
+    for (let index = 0; index < 1; index++) {
+        basic.showLeds(`
+            . . # . #
+            . # # # #
+            . # # # #
+            . . # # #
+            . . . # .
+            `)
+    }
+    for (let index = 0; index < 1; index++) {
+        basic.showIcon(IconNames.Heart)
+    }
+    for (let index = 0; index < 1; index++) {
+        basic.showLeds(`
+            # . # . .
+            # # # # .
+            # # # # .
+            # # # . .
+            . # . . .
+            `)
+    }
+    for (let index = 0; index < 1; index++) {
+        basic.showLeds(`
+            . # . . .
+            # # # . .
+            # # # . .
+            # # . . .
+            # . . . .
+            `)
+    }
+    for (let index = 0; index < 1; index++) {
+        basic.showLeds(`
+            # . . . .
+            # # . . .
+            # # . . .
+            # . . . .
+            . . . . .
+            `)
+    }
+    for (let index = 0; index < 1; index++) {
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
